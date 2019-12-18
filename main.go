bf2aa48abb53f35146c5ba1b0ff6bf2fe14d0e7c
limingzhong <limingzhong>
2019-12-18 15:24:45 +0800

    init

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"gate-ws/router"
-	"github.com/judwhite/go-svc/svc"
+	"context"
 	"net/http"
+	"time"
+	"ws-gateway/common"
+	"ws-gateway/config"
+	"ws-gateway/logger"
+	"ws-gateway/router"
+
+	"github.com/judwhite/go-svc/svc"
 )
 
 type GatewayWs struct {
@@ -11,19 +17,78 @@ type GatewayWs struct {
 }
 
 func (s *GatewayWs) Init(env svc.Environment) error {
+	config.InitConfig()
+	println("RunMode:", config.CURMODE)
+	for key, val := range config.GetSection("system") {
+		println(key, val)
+	}
+
+	// init log
+	logger.ConfigLogger(
+		config.CURMODE,
+		config.GetConfig("system", "app_name"),
+		config.GetConfig("logs", "dir"),
+		config.GetConfig("logs", "file_name"),
+		config.GetConfigInt("logs", "keep_days"),
+		config.GetConfigInt("logs", "rate_hours"),
+	)
+	println("logger init success")
+
+	// init mysql
+	dbInfo := config.GetSection("dbInfo")
+	for name, info := range dbInfo {
+		if err := common.AddDB(
+			name,
+			info,
+			config.GetConfigInt("mysql", "maxConn"),
+			config.GetConfigInt("mysql", "idleConn"),
+			time.Hour*time.Duration(config.GetConfigInt("mysql", "maxLeftTime"))); err != nil {
+			return err
+		}
+	}
+	println("mysql init success")
+
+	// init redis
+	if err := common.AddRedisInstance(
+		"",
+		config.GetConfig("redis", "addr"),
+		config.GetConfig("redis", "port"),
+		config.GetConfig("redis", "password"),
+		config.GetConfigInt("redis", "db_num")); err != nil {
+		return err
+	}
+	println("redis init success")
 
 	return nil
 }
 
 func (s *GatewayWs) Start() error {
 	s.server = &http.Server{
-		Addr: ":8087",
+		Addr:    ":8087",
 		Handler: router.NewEngine(),
 	}
+	go func() {
+		if err := s.server.ListenAndServe(); err != nil {
+			if err != http.ErrServerClosed {
+				panic(err)
+			}
+		}
+	}()
+	println("http service start")
+
 	return nil
 }
 
 func (s *GatewayWs) Stop() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.server.Shutdown(ctx); err != nil {
+		println(err.Error())
+	}
+
+	// release source
+	common.ReleaseMysqlDBPool()
+	common.ReleaseRedisPool()
 
 	return nil
 }
--- router/router.go
+++ router/router.go
@@ -1,10 +1,26 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"ws-gateway/middleware"
+
+	"github.com/gin-gonic/gin"
+)
 
 func NewEngine() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	engine := gin.New()
 
+	// use middleware here
+	engine.Use(middleware.Recover)
+	engine.Use(middleware.RequestStart)
+	engine.Use(middleware.RequestOut)
+
+	// router here
+	engine.Any("/", HealthCheck)
 	return engine
+}
+
+func HealthCheck(c *gin.Context) {
+	c.String(200, "hello world")
+	return
 }
