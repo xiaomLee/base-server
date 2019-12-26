d8a2358f46e1836a1538badbb6cacb9c0712b217
limingzhong <limingzhong>
2019-12-26 10:20:50 +0800

    fx rabbit

--- common/rabbitC_test.go
+++ common/rabbitC_test.go
@@ -11,17 +11,31 @@ import (
 func TestInitRabbit(t *testing.T) {
 	url := fmt.Sprintf("amqp://%s:%s@%s:%s%s", "dev", "dev", "127.0.0.1", "5672", "/dev")
 	if err := InitRabbit(url); err != nil {
-		t.Fatal(err)
+		t.Error(err)
+		return
 	}
 	rabbit := GetRabbitInstance()
 	rabbit.ExchangeDeclare("dev_exchange")
-	rabbit.QueueDeclare("info", "dev_exchange", "info")
-	rabbit.QueueDeclare("error", "dev_exchange", "error")
-	rabbit.QueueDeclare("warning", "dev_exchange", "warning")
+	infoQueue, err := rabbit.QueueDeclare("", "dev_exchange", "info")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	errQueue, err := rabbit.QueueDeclare("error", "dev_exchange", "error")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	warningQueue, err := rabbit.QueueDeclare("", "dev_exchange", "warning")
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
-	rabbit.Consume("info_log_1", "info", infoLog)
-	rabbit.Consume("info_log_2", "info", infoLog)
-	rabbit.Consume("error_log", "error", errorLog)
+	rabbit.Consume("info_log_1", infoQueue, infoLog)
+	rabbit.Consume("info_log_2", infoQueue, infoLog)
+	rabbit.Consume("error_log", errQueue, errorLog)
+	rabbit.Consume("warning_log", warningQueue, warningLog)
 
 	ticker := time.NewTicker(1 * time.Second)
 	timer := time.After(100 * time.Second)
@@ -43,5 +57,9 @@ func infoLog(d amqp.Delivery) {
 }
 
 func errorLog(d amqp.Delivery) {
+	fmt.Printf("error consumer[%s] msg:%s \n", d.ConsumerTag, string(d.Body))
+}
+
+func warningLog(d amqp.Delivery) {
 	fmt.Printf("error consumer[%s] msg:%s \n", d.ConsumerTag, string(d.Body))
 }
